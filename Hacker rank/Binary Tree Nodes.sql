8e290aec710d2f725c3f00b886f807e637110f2f
HazemMedhat <102475810+HazemMedhat@users.noreply.github.com>
2026-04-03 20:34:51 +0200

    Add files via upload

---
+++ Hacker rank/Binary Tree Nodes.sql
@@ -0,0 +1,11 @@
+-- Problem link  : https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+
+SELECT 
+    N,
+    CASE 
+        WHEN P IS NULL THEN 'Root'
+        WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS Node_Type
+FROM BST
+ORDER BY N;
