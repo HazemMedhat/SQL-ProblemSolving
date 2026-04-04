21591f1e8813666aebcd2f078df6f3a688f9bfe7
HazemMedhat <102475810+HazemMedhat@users.noreply.github.com>
2026-04-04 12:23:47 +0200

    Add files via upload

---
+++ Hacker rank/New Companies.sql
@@ -0,0 +1,16 @@
+-- Problem link : https://www.hackerrank.com/challenges/the-company/problem?isFullScreen=true
+
+SELECT 
+    c.company_code,
+    c.founder,
+    COUNT(DISTINCT lm.lead_manager_code)   AS lead_managers,
+    COUNT(DISTINCT sm.senior_manager_code) AS senior_managers,
+    COUNT(DISTINCT m.manager_code)         AS managers,
+    COUNT(DISTINCT e.employee_code)        AS employees
+FROM Company c
+LEFT JOIN Lead_Manager lm   ON c.company_code = lm.company_code
+LEFT JOIN Senior_Manager sm ON lm.lead_manager_code = sm.lead_manager_code
+LEFT JOIN Manager m         ON sm.senior_manager_code = m.senior_manager_code
+LEFT JOIN Employee e        ON m.manager_code = e.manager_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code;
